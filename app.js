50def49202c7a814efee9ccf93fb477ef5cce973
Seraphio <seraph@i.ua>
2016-06-06 21:49:23 +0300

    Added cordova modules

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
   var socket = require("./lib/mysocket").create();
+  console.log('APP started');
+
+/*
   socket.init({"address": "http://192.168.1.55:8080/", "actions": ["point", "connect", "disconnect"]});
 
   socket.on('connect', function () {
     console.log('connected!');
-    socket.emit('subscribe', {message: {assetId: 1}});
+    // socket.emit('subscribe', {message: {assetId: 1}});
   });
 
   socket.on('disconnect', function () {
@@ -14,3 +17,4 @@
   socket.on('point', function (data) {
     console.log('point!', data);
   });
+*/
